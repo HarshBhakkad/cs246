c17c4119df97792a73e6a1b98f4e05505c31e36e
HarshBhakkad <84085326+HarshBhakkad@users.noreply.github.com>
2022-04-02 19:30:25 +0530

    Update extendible_hashing.cpp

--- extendible_hashing.cpp
+++ extendible_hashing.cpp
@@ -8,16 +8,44 @@ private:
     /* data */
     int local_depth ;
     int size;
-    int *data = new int[size];
+    vector<int> data ;
+    int occupancy ;
 public:
     Bucket(int , int);
     ~Bucket();
+
+    int insert(int value){
+        if(occupancy < size){
+            data.push_back(value);
+            occupancy++;
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    bool search(int value){
+        return (find(data.begin(),data.end(),value)!= data.end());
+    }
+
+    bool delete_(int value){
+        vector<int>iterator:: it;
+        for(auto it = data.begin();it != data.end(); ++it){
+            if(*it == value){
+                data.erase(it);
+                return 1;
+            }
+        }
+        return 0;
+    }
 };
 
 Bucket::Bucket(int ld , int sz)
 {
     local_depth = ld;
     size = sz;
+    occupancy = 0;
 }
 
 Bucket::~Bucket()
@@ -34,12 +62,49 @@ private:
     vector<Bucket*> directory;
 public:
     hashtable(int , int);
+    int hash(int value){
+        int ans;
+        ans = (value & ((1<<global_depth)-1));
+    }
     ~hashtable();
     Bucket* createBucketPointer(int ,int);
-    insert(int );
+    bool insert(int );
 };
 
-hashtable::insert(int value)
+bool hashtable::insert(int value){
+    int hv = hash(value);
+    int status = directory[hv]->insert(value);
+    if(status == -1){
+        if(directory[hv]->local_depth == global_depth){
+            global_depth++;
+            int newhv = hash(value);
+            for(int i =0 ; i < (1<<global_depth) ; i++){
+                if( i == hv){
+                    directory.push_back(createBucketPointer(global_depth,size));
+                }
+                else{
+                directory.push_back(createBucketPointer(global_depth-1,size));
+                }
+            }
+            // hv = hash(value);
+            
+            for(int i = 0 ; i < size ; i++){
+               int temp= directory[hv]->data[i];
+               int temphv = hash(temp);
+               if(temphv != hv){
+                   directory[hv]->delete_(temp);
+                   directory[newhv]->insert(temp);
+               }
+            }
+        }
+        else{
+
+        }
+    }
+    else{
+        return true;
+    }
+}
 
 hashtable::hashtable(int gd , int sz)
 {
